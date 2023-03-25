d2706cbbb5adcb1f4a9d4264354e04f1109ff594
Stanislav Antonov <stanislav.dev@gmail.com>
2023-03-25 14:05:47 +0100

    testing established

---
+++ testing/tests.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+
+#include "munit.h"
+
+int main(int argc, const char * argv[]) {
+    printf("Hello\n");
+    return 0;
+}
