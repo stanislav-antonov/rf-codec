fd13a0b6673ef20b873cd80ab2077b2377fb8c68
Stanislav Antonov <stanislav.dev@gmail.com>
2023-03-28 15:54:10 +0200

    ccitt encoder added

---
+++ rf_codec/ccitt_whitening.c
@@ -0,0 +1,26 @@
+//
+//  Copyright © 2023 Sensory Ninja. All rights reserved.
+//
+
+#include "ccitt_whitening.h"
+
+void ccitt_whitening(uint8_t* data, uint16_t length) {
+    uint8_t whitening_key_msb = 0;
+    uint8_t whitening_key_lsb = 0xFF;  // initial value of the generator polynome GN9
+    
+    uint8_t whitening_key_lsb_result = whitening_key_lsb;
+    uint8_t whitening_key_msb_previous = 0;
+    
+    for (uint16_t j = 0; j < length - 1; j++) {
+        data[j] ^= whitening_key_lsb_result;
+        for (uint8_t i = 0; i < 8; i++) {
+            whitening_key_msb_previous = whitening_key_msb;
+            whitening_key_msb = ((whitening_key_lsb & 0x01) ^ ((whitening_key_lsb >> 5) & 0x01));
+            whitening_key_lsb = (((whitening_key_msb_previous << 7) & 0x80) | ((whitening_key_lsb >> 1) & 0xFF));
+        }
+        
+        whitening_key_lsb_result = (((whitening_key_lsb & 0xF0) >> 4) | ((whitening_key_lsb << 0x0F) << 4));
+        whitening_key_lsb_result = (((whitening_key_lsb_result & 0xCC) >> 2) | ((whitening_key_lsb_result & 0x33) << 2));
+        whitening_key_lsb_result = (((whitening_key_lsb_result & 0xAA) >> 1) | ((whitening_key_lsb_result & 0x55) << 1));
+    }
+}
---
+++ rf_codec/ccitt_whitening.h
@@ -0,0 +1,15 @@
+//
+//  Copyright © 2023 Sensory Ninja. All rights reserved.
+//
+
+#ifndef ccitt_whitening_h
+#define ccitt_whitening_h
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+
+void ccitt_whitening(uint8_t* data, uint16_t length);
+
+#endif
