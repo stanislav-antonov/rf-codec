611a6bfeed03cf9097dffe0a84da3e7c585ecf6f
Stanislav Antonov <stanislav.dev@gmail.com>
2023-04-21 13:29:06 +0200

    rs encoder - draft done

--- test/tests.c
+++ test/tests.c
@@ -7,6 +7,8 @@
 #include "ccitt_whitening.h"
 #include "hamming_codec.h"
 #include "bit_array.h"
+#include "gf.h"
+#include "rs_codec.h"
 
 void setUp(void) {}
 
@@ -16,7 +18,7 @@ void ccitt_whitening_test() {
     uint8_t data[] = { 0x11, 0x22, 0x33, 0x44 };
     ccitt_whitening(data, 4);
     uint8_t expected[] = { 0xEE, 0xA5, 0x8B, 0x1D };
-    UNITY_TEST_ASSERT_EQUAL_UINT8_ARRAY(expected, data, 4, __LINE__, NULL);
+    TEST_ASSERT_EQUAL_UINT8_ARRAY_MESSAGE(expected, data, 4, NULL);
 }
 
 void hamming_encoding_test() {
@@ -27,10 +29,10 @@ void hamming_encoding_test() {
     uint8_t source_data[] = { 0x88, 0x44, 0xCC };
     
     uint16_t encoded_data_length = hamming_encode(source_data, data_length, dw_bits_count, encoded_data);
-    UNITY_TEST_ASSERT_EQUAL_INT16(6, encoded_data_length, __LINE__, NULL);
+    TEST_ASSERT_EQUAL_UINT16(6, encoded_data_length);
     
     uint8_t expected[] = { 0x70, 0x38, 0x93, 0xC9, 0xE3, 0x01 };
-    UNITY_TEST_ASSERT_EQUAL_UINT8_ARRAY(expected, encoded_data, encoded_data_length, __LINE__, NULL);
+    TEST_ASSERT_EQUAL_UINT8_ARRAY_MESSAGE(expected, encoded_data, encoded_data_length, NULL);
 }
 
 void hamming_decoding_test() {
@@ -41,10 +43,10 @@ void hamming_decoding_test() {
     uint8_t source_data[] = { 0x70, 0x38, 0x93, 0xC9, 0xE3, 0x01 };
     
     uint16_t decoded_data_length = hamming_decode(source_data, data_length, dw_bits_count, decoded_data);
-    UNITY_TEST_ASSERT_EQUAL_INT16(3, decoded_data_length, __LINE__, NULL);
+    TEST_ASSERT_EQUAL_UINT16(3, decoded_data_length);
     
     uint8_t expected[] = { 0x88, 0x44, 0xCC };
-    UNITY_TEST_ASSERT_EQUAL_UINT8_ARRAY(expected, decoded_data, decoded_data_length, __LINE__, NULL);
+    TEST_ASSERT_EQUAL_UINT8_ARRAY_MESSAGE(expected, decoded_data, decoded_data_length, NULL);
 }
 
 void hamming_encoding_decoding_no_errors_test() {
@@ -62,7 +64,30 @@ void hamming_encoding_decoding_no_errors_test() {
     
     uint16_t decoded_data_length = hamming_decode(encoded_data, data_length, dw_bits_count, decoded_data);
     
-    UNITY_TEST_ASSERT_EQUAL_UINT8_ARRAY(source_data, decoded_data, decoded_data_length, __LINE__, NULL);
+    TEST_ASSERT_EQUAL_UINT8_ARRAY_MESSAGE(source_data, decoded_data, decoded_data_length, NULL);
+}
+
+void galois_field_ops_test() {
+    uint8_t mult_result = gf_mult(137, 42);
+    TEST_ASSERT_EQUAL_UINT8(195, mult_result);
+}
+
+void reed_solomon_encoding_test() {
+    uint16_t msg_in_len = 16;
+    uint8_t msg_in[] = {0x40, 0xd2, 0x75, 0x47, 0x76, 0x17, 0x32, 0x06, 0x27, 0x26, 0x96, 0xc6, 0xc6, 0x96, 0x70, 0xec};
+    
+    uint8_t nsym = 10;
+    uint8_t msg_out_len_expected = nsym + msg_in_len;
+    uint8_t msg_out[msg_out_len_expected];
+    memset(msg_out, 0, sizeof(msg_out));
+    
+    uint8_t msg_out_expected[] = {0x40, 0xd2, 0x75, 0x47, 0x76, 0x17, 0x32, 0x06, 0x27, 0x26, 0x96, 0xc6, 0xc6, 0x96, 0x70, 0xec, 0xbc, 0x2a, 0x90, 0x13, 0x6b, 0xaf, 0xef, 0xfd, 0x4b, 0xe0 };
+    
+    uint16_t msg_out_len;
+    rs_encode(nsym, msg_in, msg_in_len, msg_out, &msg_out_len);
+    
+    TEST_ASSERT_EQUAL_UINT8(msg_out_len_expected, msg_out_len);
+    TEST_ASSERT_EQUAL_UINT8_ARRAY_MESSAGE(msg_out_expected, msg_out, msg_out_len, NULL);
 }
 
 int main(int argc, const char * argv[]) {
@@ -72,6 +97,11 @@ int main(int argc, const char * argv[]) {
     hamming_encoding_test();
     hamming_decoding_test();
     hamming_encoding_decoding_no_errors_test();
+    
+    rs_init();
+    
+    galois_field_ops_test();
+    reed_solomon_encoding_test();
     
     return UNITY_END();
 }
